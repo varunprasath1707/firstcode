f2d2664d1f0bd67a3cbcc083f5e21a538aac013b
varunprasath1707 <129736495+varunprasath1707@users.noreply.github.com>
2023-04-03 15:41:46 +0530

    add

---
+++ demo.java
@@ -0,0 +1,19 @@
+package Main;
+
+import java.util.Scanner;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		int a,b;
+		int c;
+		Scanner Scanner =new Scanner(System.in);
+		a=Scanner.nextInt();
+		b=Scanner.nextInt();
+		c=a+b;
+		System.out.println(c);
+	}
+
+}
